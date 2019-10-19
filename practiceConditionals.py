2e37e38db37c4a2a370d1b90537b3ce7b3e3aa91
miguelabragonz <55327101+miguelabragonz@users.noreply.github.com>
2019-10-19 17:43:35 -0500

    Initial commit

---
+++ practiceConditionals.py
@@ -0,0 +1,12 @@
+'''
+Created on Oct 19, 2019
+
+@author: ITAUser
+'''
+x = 7
+if x > 0:
+    print("The x value is positive")
+elif x == 0:
+    print("The x value is zero")
+else:
+    print("The x value is negative")
